7301c6a122d57fc7831a8696265118596d0b3fdc
Caio Roberto <caiorrs@gmail.com>
2021-10-09 09:41:00 -0300

    Renamed project

--- android/app/src/main/java/com/ihc/moviegram/MainActivity.java
+++ android/app/src/main/java/com/ihc/moviegram/MainActivity.java
@@ -1,4 +1,4 @@
-package com.moviestest;
+package com.ihc.moviegram;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "moviesTest";
+    return "MovieGram";
   }
 }
